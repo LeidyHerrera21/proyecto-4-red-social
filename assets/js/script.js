912edb541176584d8238fe232ee79d8196c2cae5
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-17 17:42:49 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,49 @@
-import { onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+const cvForm = document.getElementById('cv-form');
 
-import { auth} from "./firebase/firebase.js"
-import {loginCheck} from  './firebase/login_check.js';
+cvForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fileInput = document.getElementById('cv');
+    const file = fileInput.files[0];
+
+    if (file && (file.type === 'application/pdf' || file.type === 'application/msword' || file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')) {
+        alert('Gracias por enviar tu currículum. Nos pondremos en contacto contigo pronto.');
+        cvForm.reset();
+    } else {
+        alert('Por favor, sube un archivo válido (PDF o Word).');
+    }
+});
+
+const heroImages = document.querySelectorAll('.hero-image');
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+
+    heroImages[currentImageIndex].classList.remove('active');
 
 
-import './firebase/signup_form.js';
-import './firebase/signin_form.js';
-import './firebase/googleLogin.js';
-import './firebase/logout.js';
-import './firebase/notas.js';
+    currentImageIndex = (currentImageIndex + 1) % heroImages.length;
 
-onAuthStateChanged( auth, async (user) => {
-    
-    loginCheck(user)
-    if (user) {
-       loginCheck(user);
 
-       import('./firebase/setup_tasks.js')
-        .then(({default: septuTasks}) => septuTasks(user));
-   } 
-   
-   // Si ha salido
-   else {
-       loginCheck(user);
-   }
+    heroImages[currentImageIndex].classList.add('active');
+}
 
-})
+setInterval(changeBackgroundImage, 5000);
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { 
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+const aboutTitles = document.querySelectorAll('.about-title');
+
+aboutTitles.forEach(title => {
+    title.addEventListener('click', () => {
+        const aboutItem = title.parentElement; 
+        aboutItem.classList.toggle('active'); 
+    });
+});
