37d22f2e861795931ba5868017d3fd585ba6d369
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-17 19:02:54 -0500

    Update daltonismo.js

--- assets/js/daltonismo.js
+++ assets/js/daltonismo.js
@@ -14,3 +14,11 @@ document.getElementById("modo-daltonismo").addEventListener("click", () => {
 if (localStorage.getItem("modoDaltonismo") === "activado") {
     document.body.classList.add("daltonismo");
 }
+
+// JavaScript para Activar Modo Daltonismo//
+function toggleColorblindMode() {
+        document.body.classList.toggle("colorblind-mode");
+    }
+
+// Botón para Activar Modo Daltonismo //
+<button onclick="toggleColorblindMode()">Modo Daltonismo</button>
