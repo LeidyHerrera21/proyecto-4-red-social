ec2cfda96442613c8dcbf79e78cd982a6b7e075d
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-19 13:12:42 -0500

    Create reducir.js

---
+++ assets/js/reducir.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAumentarTexto = document.getElementById("btn-reducir-texto");
+    let fontSize = 16;
+
+    btnAumentarTexto.addEventListener("click", function () {
+        fontSize -= 2;
+        document.body.style.fontSize = fontSize + "px";
+    });
+});
