0c2ffb615b9abdcf2025d81ca933f635d9a47b66
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-19 11:26:37 -0500

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,29 +1,29 @@
 // Importar Firebase desde la CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
 
 // Configuración de Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBTLAhAUZyK2Hfw4lySSJuRaSJ9S8yKa3M",
     authDomain: "grupofundades-96cfa.firebaseapp.com",
     projectId: "grupofundades-96cfa",
-    storageBucket: "grupofundades-96cfa.appspot.com",
+    storageBucket: "grupofundades-96cfa.firebasestorage.app", // Corregido
     messagingSenderId: "11636375123",
-    appId: "1:11636375123:web:2fbe1610eb85baa19650a6",
-    measurementId: "G-749PF5EWP1"
+    appId: "1:11636375123:web:67a29b9e9fc1b1989650a6",
+    measurementId: "G-HC27GER130"
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
+const auth = getAuth(app); // Se agregó la importación
 const provider = new GoogleAuthProvider();
 
 // Esperar a que el DOM cargue
 document.addEventListener("DOMContentLoaded", () => {
     const loginButton = document.getElementById("login-google");
-    
+
     if (loginButton) {
         loginButton.addEventListener("click", () => {
             signInWithPopup(auth, provider)
