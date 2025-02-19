460d807e068726acc5034ea23e1464706c71af59
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-19 14:20:43 -0500

    Update reducir.js

--- assets/js/reducir.js
+++ assets/js/reducir.js
@@ -2,8 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnReducirTexto = document.getElementById("btn-reducir-texto");
     let fontSize = 16;
 
-    btnAumentarTexto.addEventListener("click", function () {
-        fontSize -= 2;
-        document.body.style.fontSize = fontSize + "px";
+
+// Reducir tamaño del texto
+    btnReducirTexto.addEventListener("click", function () {
+        if (fontSize > 10) {
+            fontSize -= 2;
+            document.body.style.fontSize = fontSize + "px";
+        }
     });
-});
