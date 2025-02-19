a6e633abe5d3f75da1a5bbc27ee3dc9c37565ab0
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-19 14:01:25 -0500

    Update aumento.js

--- assets/js/aumento.js
+++ assets/js/aumento.js
@@ -1,19 +1,9 @@
- document.addEventListener("DOMContentLoaded", function () {
-      const btnAumentarTexto = document.getElementById("btn-aumentar-texto");
-      const btnReducirTexto = document.getElementById("btn-reducir-texto");
-      let fontSize = 16;
-      const contenido = document.getElementById("contenido");
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAumentarTexto = document.getElementById("btn-aumentar-texto");
+    let fontSize = 16;
 
-            btnAumentarTexto.addEventListener("click", function () {
-                fontSize += 2;
-                contenido.style.fontSize = fontSize + "px";
-            });
-
-            btnReducirTexto.addEventListener("click", function () {
-                if (fontSize > 10) { // Evita que el texto sea demasiado pequeño
-                    fontSize -= 2;
-                    contenido.style.fontSize = fontSize + "px";
-                }
-            });
- });
-
+    btnAumentarTexto.addEventListener("click", function () {
+        fontSize -= 2;
+        document.body.style.fontSize = fontSize + "px";
+    });
+});
